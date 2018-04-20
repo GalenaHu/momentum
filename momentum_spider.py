b519fc25b00cf0d9d920d33c2f280411eaf0df40
GalenaHu <37532132+GalenaHu@users.noreply.github.com>
2018-04-20 15:11:11 +0800

    Update momentum_spider.py

--- momentum_spider.py
+++ momentum_spider.py
@@ -1,21 +1,25 @@
 # coding: utf-8
 import re
 import os
-import urllib
-from multiprocessing.pool import ThreadPool
+import urllib.request
+from multiprocessing import Pool
+import time
 
-MOMENTUM_URL = 'https://d3cbihxaqsuq0s.cloudfront.net/'
+momentum_url = 'https://d3cbihxaqsuq0s.cloudfront.net/'
 
 
 def spider():
-    context = urllib.urlopen(MOMENTUM_URL).read()
+    context = str(urllib.request.urlopen(momentum_url).read())
     return context
 
 
 def parse_context(c):
+#   c = c.decode('utf-8')
     image_keys = re.findall(r'\<Key\>(.+?)\<\/Key\>', c)
+    '''for key in image_keys:
+        print([momentum_url + key])'''
     return [
-        MOMENTUM_URL + key for key in image_keys
+        momentum_url + key for key in image_keys
     ]
 
 
@@ -23,30 +27,30 @@ def main():
     context = spider()
     images = parse_context(context)[1:]
 
-    pool = ThreadPool(6)
-    pool.map(save_img, images)
-    pool.close()
-    pool.join()
-    
+    p = Pool(6)
+    p.map(save_img, images)
+    p.close()
+    p.join()
 
-def save_img(img_url, file_path='/Users/ziv/Downloads/momentums'):
+
+def save_img(img_url, file_path = 'D:\picture\mumentum'):
     try:
         if not os.path.exists(file_path):
             os.makedirs(file_path)
         file_suffix = os.path.basename(img_url)
-        filename = '{}{}{}'.format(file_path, os.sep, file_suffix)
-        urllib.urlretrieve(img_url, filename=filename)
-        print filename
+        filename = '{}{}{}'.format(file_path, os.path.sep, file_suffix)
+        urllib.request.urlretrieve(img_url, filename=filename)
+        print(filename)
     except IOError as e:
-        print e
+        print(e)
     except Exception as e:
-        print e
+        print(e)
 
 
 if __name__ == '__main__':
-    import time
+
+    print('start...')
     s = time.time()
     main()
-    print 'finished.'
-    print time.time() - s
-
+    print('finished')
+    print(time.time() - s)
